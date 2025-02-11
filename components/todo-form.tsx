15b6a0f84a531d360c6c66899e896892c75a7b64
root <root@DESKTOP-6FT5ETI>
2025-02-11 21:19:53 +0900

    fix: Restore useEffect hooks for form state management

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -1,4 +1,4 @@
-import { useForm, useFormContext } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Todo } from "@/types/todo"
 import { TodoFormValues, todoFormSchema } from "@/lib/schemas"
